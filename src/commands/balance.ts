2af015c7efe391fec20d95149744bd666e0fed11
arekkusu <bokuwaarekkusu66@gmail.com>
2024-02-24 00:26:51 +0200

    I added the /balance command

---
+++ src/commands/balance.ts
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder  , EmbedBuilder , CommandInteraction } from 'discord.js';
+
+import db from '../db/db.js';
+import { bottable } from '../db/schema.js';
+import { eq } from 'drizzle-orm';
+
+const command = {
+    data: new SlashCommandBuilder()
+        .setName('balance')
+        .setDescription('the total amount of coins you have'),
+
+    execute: async(interaction: CommandInteraction) => {
+
+        const currentAmount = await db.select({ amount : bottable.amount }).from(bottable).where(eq(bottable.id , interaction.user.id));
+
+        const embed = new EmbedBuilder()
+        .setColor(0x00ee00)
+        .setDescription(`you got ${currentAmount[0]?.amount || 0} coins!`);
+
+        await interaction.reply({ embeds: [embed] });
+    }
+};
+
+export default command;
