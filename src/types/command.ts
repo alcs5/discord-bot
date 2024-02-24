1b345114dd860f445836fe898ba3a2fd52d8ea87
arekkusu <bokuwaarekkusu66@gmail.com>
2024-02-25 01:17:55 +0200

    added the cooldown property on the cooldown type

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,6 +1,7 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 export type Command = {
+    cooldown?: number,
     execute : (interaction : ChatInputCommandInteraction) => any;
     data: SlashCommandBuilder
 };
