e0d65800f8496fb2890c309f37cc7488f1d82e61
arekkusu <bokuwaarekkusu66@gmail.com>
2024-02-26 20:51:22 +0200

    added embeds for the command

--- src/commands/bet.ts
+++ src/commands/bet.ts
@@ -1,11 +1,11 @@
-import { SlashCommandBuilder , EmbedBuilder , type CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder , EmbedBuilder , type CommandInteraction, ColorResolvable } from 'discord.js';
 
 import db from '../db/db.js';
 import { bottable } from '../db/schema.js';
 import { eq } from 'drizzle-orm';
 
 const command = {
-    cooldown: 30,
+    // cooldown: 30,
 
     data: new SlashCommandBuilder()
         .setName('bet')
@@ -30,9 +30,19 @@ const command = {
         const currentAmount = await db.select({ amount: bottable.amount }).from(bottable).where(eq(bottable.id , interaction.user.id));
         const rightAmount = currentAmount?.[0]?.amount || 0;
 
+        if (rightAmount === 0) {
+            await interaction.reply({ embeds: [embed(0xFF1111 , 'You have 0 coins!' , 'Use the command /give to give you some coins')] });
+            return;
+        };
+
 
         async function updateAmount(value : number) {
             await db.insert(bottable).values({ id: interaction.user.id , amount: rightAmount + value < 0 ? 0 : rightAmount + value }).onConflictDoUpdate({ target: bottable.id , set: { amount: rightAmount + value < 0 ? 0 : rightAmount + value }});
+        };
+
+        function embed(color : ColorResolvable , title : string , description : string) : EmbedBuilder {
+            const embed = new EmbedBuilder().setColor(color).setTitle(title).setDescription(description);
+            return embed;
         };
 
 
@@ -47,12 +57,17 @@ const command = {
 
         if (chance === 'big') {
 
+            if (rightAmount < 30) {
+                await interaction.reply({ embeds: [embed(0xFF1111 , 'You dont have enough coins to bet!' , 'Use the give command to get some')] });
+                return;
+            };
+
             if (randomBig === betNumber) {
-                await interaction.reply(`you guessed the number ${randomBig} right!`);
+                await interaction.reply({ embeds: [embed(0x00EE00 , `You guessed the number ${randomBig} right!` , 'You won 30 coins!')] });
                 await updateAmount(+30);
             }
             else {
-                await interaction.reply(`you didnt guessed right! , the number was ${randomBig}`);
+                await interaction.reply({ embeds: [embed(0xEE0000 , `You guessed wrong! the number was ${randomBig}` , 'You lost 30 coins!')] });
                 await updateAmount(-30);
             };
         };
@@ -69,12 +84,18 @@ const command = {
 
         if (chance === 'small') {
 
+            if (rightAmount < 60) {
+                await interaction.reply({ embeds: [embed(0xFF1111 , 'You dont have enough coins to bet!' , 'Use the give command to get some')] });
+                return;
+            };
+
             if (randomSmall === betNumber) {
-                await interaction.reply(`you guessed the number ${randomSmall} right!`);
+
+                await interaction.reply({ embeds: [embed(0x00EE00 , `You guessed the number ${randomSmall} right!` , 'You won 60 coins!')] });
                 await updateAmount(+60);
             } 
             else {
-                await interaction.reply(`you didnt guessed right! , the number was ${randomSmall}`);
+                await interaction.reply({ embeds: [embed(0xEE0000 , `You guessed wrong! the number was ${randomSmall}` , 'You lost 60 coins!')] });
                 await updateAmount(-60);
             };
         };
