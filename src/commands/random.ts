219f1e8d1313c3ca6056750dfe4ce4606c053627
arekkusu <bokuwaarekkusu66@gmail.com>
2024-03-03 00:26:28 +0200

    i just added a new command

---
+++ src/commands/random.ts
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder , EmbedBuilder , CommandInteraction } from 'discord.js';
+
+
+const command = {
+    data: new SlashCommandBuilder()
+        .setName('random')
+        .setDescription('generate a random number')
+        .addIntegerOption((option) => 
+            option
+                .setName('min').setDescription('the minimum number to generate from').setRequired(true)
+        )
+        .addIntegerOption((option) =>
+            option
+                .setName('max').setDescription('the maximum number to genereate from').setRequired(true)
+        ),
+
+        execute: async (interaction : CommandInteraction) => {
+            const min = +interaction.options.get('min').value;
+            const max = +interaction.options.get('max').value;
+
+            const embed = new EmbedBuilder().setColor(0x0000aa).setTitle(`Your number is ${Math.floor(Math.random() * (max - min) + 1) + min}`);
+
+            await interaction.reply({ embeds: [embed] });
+        }
+};
+
+
+export default command;
